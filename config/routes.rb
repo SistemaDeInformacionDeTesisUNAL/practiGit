56aab77217771d3d6b0cdb2c6df3a5aeda31c5b6
Jhonattan <jjrodriguezn@unal.edu.co>
2017-02-12 11:44:50 -0500

    route to contactUS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #get 'pages/index'
   root to: 'pages#index'
-
+  get 'contactUS', to: 'pages#contactUS'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
